d4957593149544c7dd97e701cb8cd96a8ded1c98
JipV <jip--pie@ghotmail.com>
2015-05-19 16:25:56 +0200

    Vanalles

--- app/js/app.js
+++ app/js/app.js
@@ -3,5 +3,10 @@ require('angular/angular');
 // Create your app
 var app = angular.module("Mahjong_Mayhem", []);
 
+var urlFactory = require("./factories/urlFactory");
+var gamesFactory = require("./factories/gamesFactory");
 var gamesController = require("./controllers/GamesController");
+
+app.factory("urlFactory", urlFactory);
+app.factory("gamesFactory", gamesFactory);
 app.controller("GamesController", gamesController);
